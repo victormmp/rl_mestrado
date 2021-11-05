3f8716dcfde7493595e9ec08a1862bacf5a2b8d4
Victor Magalhaes <victormmp@outlook.com>
2021-11-05 00:45:32 -0300

    Created Actor Critic and created notebook to aggregate results

--- rl_mestrado/model/simple_network.py
+++ rl_mestrado/model/simple_network.py
@@ -13,10 +13,8 @@ class SimpleNetwork(nn.Module):
         super().__init__()
 
         self.linear1    = nn.Linear(in_features=features, out_features=64)
-        self.batch_n1   = nn.BatchNorm1d(num_features=self.linear1.out_features)
         self.relu1      = nn.LeakyReLU()
         self.linear2    = nn.Linear(in_features=64, out_features=32)
-        self.batch_n2   = nn.BatchNorm1d(num_features=self.linear2.out_features)
         self.relu2      = nn.LeakyReLU()
         self.output1    = nn.Linear(in_features=32, out_features=outputs)
         self.softmax1   = nn.Softmax(dim=0)
---
+++ rl_mestrado/scripts/run_actor_critic_parallel.py
@@ -0,0 +1,122 @@
+import os
+from copy import deepcopy
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+import torch
+from joblib import Parallel, delayed
+from rl_mestrado.agent.actor_critic import ActorCriticAgentLearner
+from rl_mestrado.tools.log import get_logger
+
+#=======================================================| VARIABLES
+
+DATA_PATH = os.path.join('silver', 'daily_feature_set.csv')
+NAME_SUFFIX = "daily_v2"
+ASSETS = ['SPY', 'TLT.O', 'XLK']
+MODEL_OUTPUT_PATH = os.path.join('results')
+BACKTEST_OUTPUT_PATH = os.path.join('results', 'backtest')
+TRAIN_OUTPUT_PATH = os.path.join('results', 'portfolio_values')
+START_OUT_SAMPLE = '2015-01-05'
+END_OUT_SAMPLE = '2021-09-30'
+N_FEATURES = 14
+EPOCHS = 50000
+
+#=======================================================| TRAIN
+
+data = pd.read_csv(DATA_PATH, parse_dates=True, index_col=0)
+data = data.loc[(~data['TLT.O_logReturns'].isnull()) | (~data['TAIL.K_logReturns'].isnull())]
+data.fillna(0, inplace=True)
+print(data.columns)
+
+start_out_samp = pd.Timestamp(START_OUT_SAMPLE)
+end_out_samp = pd.Timestamp(END_OUT_SAMPLE)
+
+df_in_sample = data.loc[:start_out_samp, :]
+df_out_sample = data.loc[start_out_samp:end_out_samp, :]
+
+
+def run(**kwargs):
+
+    actor_lr = kwargs.get('learning_rate', 1e-3)
+    critic_lr = actor_lr * 10
+    n_epochs = kwargs.get('epochs', EPOCHS)
+    df_in_sample = deepcopy(kwargs.get('df_in_sample'))
+    df_out_sample = deepcopy(kwargs.get('df_out_sample'))
+
+    agent  = ActorCriticAgentLearner(
+    actor_lr=actor_lr,
+    critic_lr=critic_lr,
+    assets=ASSETS,
+    n_features=N_FEATURES,
+    n_assets=len(ASSETS),
+    name_suffix=f"daily_actor_lr_{actor_lr}_critic_lr_{critic_lr}_epoch_{n_epochs}"
+)
+
+    model_path, train_df = agent.train(
+        data=df_in_sample, 
+        epochs=n_epochs, 
+        result_output_path=MODEL_OUTPUT_PATH
+    )
+
+    #=======================================================| BACKTEST
+    print('\n#====================| PERFORMING BACKTEST |')
+
+
+    port_value = 0.0
+    backtest_df = []
+
+    columns_to_drop = [c for c in df_out_sample.columns if 'TAIL' in c]
+    df_out_sample = df_out_sample.drop(columns_to_drop, axis=1)
+
+    first_date = df_out_sample.iloc[0, :]
+    state = first_date.values
+    weights = agent.act(state)
+
+    df_out_sample.iloc[1:, :]
+
+    for date, row in df_out_sample.iterrows():
+        port_value += np.dot(weights, row[[c + '_logReturns' for c in ASSETS]].values)
+        backtest_df.append((date, np.exp(port_value)))
+
+        state = row.values
+        weights = agent.act(state)
+
+    backtest_df = pd.DataFrame(backtest_df, columns=['date', agent._agent_name]).set_index('date')
+
+    backtest_result_path = os.path.join(BACKTEST_OUTPUT_PATH, agent._agent_name+'.csv')
+    backtest_df.to_csv(backtest_result_path, index=True)
+
+    print(f"Backtest result saved at {backtest_result_path}")
+
+    return backtest_df, train_df
+
+configs = [
+    (1e-6, EPOCHS, df_in_sample, df_out_sample),
+    (1e-5, EPOCHS, df_in_sample, df_out_sample),
+    (1e-4, EPOCHS, df_in_sample, df_out_sample),
+    (1e-3, EPOCHS, df_in_sample, df_out_sample)
+]
+
+results = Parallel(n_jobs=-2)(
+    delayed(run)(
+            learning_rate=lr,
+            epochs=e,
+            df_in_sample=df_i,
+            df_out_sample=df_o
+    )
+    for lr, e, df_i, df_o in configs
+)
+
+df_result_backtest, df_result_train = zip(*results)
+
+print("\n\nFINISHED PARALLEL EXECUTION\n")
+result_path = os.path.join(TRAIN_OUTPUT_PATH, dt.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")+f'_aggregated_{NAME_SUFFIX}.csv')
+pd.concat(df_result_train, axis=1).to_csv(result_path, index=True)
+print(f"Train result saved at {result_path}")
+
+result_path = os.path.join(BACKTEST_OUTPUT_PATH, dt.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")+f'_aggregated_{NAME_SUFFIX}.csv')
+pd.concat(df_result_backtest, axis=1).to_csv(result_path, index=True)
+print(f"Backtest result saved at {result_path}")
+
+
